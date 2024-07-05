6e22a5f9fdef0ccd57ee95edd632caf219083ffd
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-05 10:22:01 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240705/데이터 교환/exchange-data.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c = 7;
+        int tempA = a;
+
+        a = c;
+        c = b;
+        b = tempA;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
