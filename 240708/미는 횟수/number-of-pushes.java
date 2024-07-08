70cda2110804bcaa3bea21c6e7cad251ba8a04de
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-08 12:59:11 +0900

    Title: 미는 횟수, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240708/미는 횟수/number-of-pushes.java
+++ 240708/미는 횟수/number-of-pushes.java
@@ -8,13 +8,13 @@ public class Main {
         String B = sc.nextLine();
         boolean flag = true;
 
-        for(int i = 0; i <= A.length(); i++){
+        for(int i = 0; i < A.length(); i++){
             if(A.equals(B)){
                 System.out.println(i);  
                 flag = false;  
                 break;
             }
-            A = A.substring(1) + A.substring(0, 1);
+            B = B.substring(1) + B.substring(0, 1);
         }
 
         if(flag){
