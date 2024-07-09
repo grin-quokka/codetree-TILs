f40c3cf624d8569e6cfeb14eecb48590e6cb32ad
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-09 17:54:43 +0900

    Title: 2021년 날짜의 유무, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240709/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        if(isExistMD(M, D)){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
+    }
+
+    public static boolean isExistMD(int M, int D){
+        if(M > 12){
+            return false;
+        }
+
+        if(D > 31){
+            return false;
+        }
+
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if(days[M] < D){
+            return false;
+        }
+
+        return true;
+    }
+}
