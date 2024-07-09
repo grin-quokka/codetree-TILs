c3f555c39bfaf3d0a795ff91a58cd7227ef65d65
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-09 18:11:41 +0900

    Title: 그 계절, 그 날, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240709/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Y = sc.nextInt();
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        if(isExist(Y, M, D)){
+            System.out.print(getSeason(M));
+        }else{
+            System.out.print(-1);
+        }
+    }
+
+    public static boolean isExist(int Y, int M, int D){
+        if(getLastDay(Y, M, D) < D){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static int getLastDay(int Y, int M, int D){
+        if(M == 2 ){
+            if(isLeapYear(Y)){
+                return 29;
+            }
+
+            return 28;
+        }
+
+        if(M == 4 || M == 6 && M == 9 || M == 11){
+            return 30;
+        }
+
+        return 31;
+    }
+
+    public static boolean isLeapYear(int Y){
+        if(Y % 4 != 0){
+            return false;
+        }
+
+        if(Y % 100 == 0){
+            if(Y % 400 == 0){
+                return true;
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    public static String getSeason(int M){
+        if(M == 12){
+            return "Winter";
+        }
+
+        if(M >= 9){
+            return "Fall";
+        }
+
+        if(M >= 6){
+            return "Summer";
+        }
+
+        if(M >= 3){
+            return "Spring";
+        }
+
+        return "Winter";
+    }
+}
