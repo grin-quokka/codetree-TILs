982c88f3f6d7b79d0dc4b5b09a095d2d767f6477
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-13 16:00:08 +0900

    Title: 문자열 정렬, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240713/문자열 정렬/string-sort.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] cArr = str.toCharArray();
+        Arrays.sort(cArr);
+        System.out.print(new String(cArr));
+
+    }
+}
