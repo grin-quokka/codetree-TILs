e96d8e41623579e11c6c93fa5b31e6a3563a53bf
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-13 11:14:02 +0900

    Title: Factorial, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240713/Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(fac(n));
+    }
+
+    public static int fac(int n){
+        if(n == 1){
+            return 1;
+        }
+        return fac(n - 1) * n;
+    }
+}
