1123c85f32e2fffba21b1b25ce23f04213e1bdb1
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-16 18:10:27 +0900

    Title: 007, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240716/007/007.java
+++ 240716/007/007.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 
+class Spy {
+    String secretCode;
+    char meetingPoint;
+    int time;
+
+    Spy(String secretCode, char meetingPoint, int time){
+        this.secretCode = secretCode;
+        this.meetingPoint = meetingPoint;
+        this.time = time;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,17 +20,5 @@ public class Main {
         System.out.println("secret code : " + spy.secretCode);
         System.out.println("meeting point : " + spy.meetingPoint);
         System.out.println("time : " + spy.time);
-    }
-
-    public static class Spy {
-        String secretCode;
-        char meetingPoint;
-        int time;
-
-        public Spy(String secretCode, char meetingPoint, int time){
-            this.secretCode = secretCode;
-            this.meetingPoint = meetingPoint;
-            this.time = time;
-        }
     }
 }
