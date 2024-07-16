28a277a562af47d33f33a4596a2c3246c52128ab
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-16 10:05:13 +0900

    Title: k번째로 신기한 문자열, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String T = sc.next();
+        String[] arr = new String[n];
+
+        for(int i = 0; i < n; i++){
+            String word = sc.next();
+            if(word.startsWith(T)){
+                arr[i] = word;
+            }
+        }
+
+
+        Arrays.sort(arr, Comparator.nullsLast(Comparator.naturalOrder()));
+        System.out.print(arr[k - 1]);
+    }
+}
