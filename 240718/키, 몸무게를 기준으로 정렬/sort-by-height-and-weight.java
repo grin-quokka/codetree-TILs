4f751d69ea55e4cc67d78033ad9c8684088b6f33
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2024-07-18 17:25:58 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240718/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Student implements Comparable<Student>{
+    String name;
+    int height, weight;
+
+    Student(String name, int height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Student student){
+        if(this.height != student.height){
+            return this.height - student.height;
+        }
+
+        return student.weight - this.weight;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for(int i = 0; i < n; i++){
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+            students[i] = new Student(name, height, weight);
+        }
+
+        Arrays.sort(students);
+
+        for(int i = 0; i < n; i++){
+            Student s = students[i];
+            System.out.println(s.name + " " + s.height + " " + s.weight);
+        }
+    }
+}
